787e1533a1ad88aa6af4625a230b43014a488715
tuyenv <bitworkvn@gmail.com>
2019-12-25 09:02:37 +0700

    api

---
+++ api/users/models/users.model.js
@@ -0,0 +1,51 @@
+const userSchema = new Schema({
+    firstName: String,
+    lastName: String,
+    email: String,
+    password: String,
+    permissionLevel: Number
+ });
+
+exports.createUser = (userData) => {
+    const user = new User(userData);
+    return user.save();
+};
+
+exports.findById = (id) => {
+    return User.findById(id).then((result) => {
+        result = result.toJSON();
+        delete result._id;
+        delete result.__v;
+        return result;
+    });
+};
+
+exports.patchUser = (id, userData) => {
+    return new Promise((resolve, reject) => {
+        User.findById(id, function (err, user) {
+            if (err) reject(err);
+            for (let i in userData) {
+                user[i] = userData[i];
+            }
+            user.save(function (err, updatedUser) {
+                if (err) return reject(err);
+                resolve(updatedUser);
+            });
+        });
+    })
+};
+
+exports.list = (perPage, page) => {
+    return new Promise((resolve, reject) => {
+        User.find()
+            .limit(perPage)
+            .skip(perPage * page)
+            .exec(function (err, users) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(users);
+                }
+            })
+    });
+};
